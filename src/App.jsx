0641543235ddc97c9a8d4ed3503dde2eaddb0595
Maryjune Ortega <ortegamaryjunet@gmail.com>
2026-09-02 16:10:21 +0800

    created header and navbar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+  
+    <div className="min-h-screen bg-white-950 text-white">
+        <Header />
+
+        <main className="pt-20"></main>
+
+    </div>
+   
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <nav className="fixed top-0 left-0 right-0 z-50 bg-black p-4">
+            <div className="container mx-auto flex items-center justify-between">
+
+                <div className="flex items-center gap-3">
+                    <img src="/spotify-logo.png" alt="logo" className="h-10 w-10 object-contain"/>
+                    <div className="text-3xl font-bold tracking-tight text-white hover:cursor-pointer hover:text-green-600">Spotify Track Playlist Manager</div>
+                </div>
+
+                <div className="flex items-center gap-4 text-xl">
+                    <button className="px-4 py-2 tracking-tight font-bold text-white hover:text-green-600">Add New Track</button>
+                    <button className="px-4 py-2 tracking-tight font-bold text-white hover:text-green-600">Tracks</button>
+                </div>
+            </div>
+        </nav>
+    );
+}
+export default Header;
